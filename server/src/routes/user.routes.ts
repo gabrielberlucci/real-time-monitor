c69857c85b1f1f7ca2b55a795921776f0f449ff3
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-03 17:16:15 -0300

    feat: add validateData middleware

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,8 +1,10 @@
 import { registerUser } from '@/controllers/user.controller';
+import { validateData } from '@/middleware/validation/validation.middleware';
+import { userRegisterSchema } from '@/schemas/user.schema';
 import { Router } from 'express';
 
 const userRouter: Router = Router();
 
-userRouter.post('/register', registerUser);
+userRouter.post('/register', validateData(userRegisterSchema), registerUser);
 
 export { userRouter };
