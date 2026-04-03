29fd202f8aceca5dae7eb5c8f3761151e59a82c8
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-03 17:17:26 -0300

    feat: add userLoginSchema

--- server/src/schemas/user.schema.ts
+++ server/src/schemas/user.schema.ts
@@ -1,7 +1,12 @@
-import { z } from 'zod';
+import { email, z } from 'zod';
 
 export const userRegisterSchema = z.object({
   email: z.email(),
   name: z.string(),
   password: z.string().min(8),
+});
+
+export const userLoginSchema = z.object({
+  email: z.string(),
+  password: z.string(),
 });
