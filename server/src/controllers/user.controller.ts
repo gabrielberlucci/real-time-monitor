ca894437bd77bd4df303ad51b3fa9f276f84db4e
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-04 00:28:12 -0300

    feat: add error validation to both register and login routes

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,4 +1,6 @@
-import { createUser } from '@/services/user.service';
+import { ConflictError } from '@/error/ConflictError';
+import { UnauthorizedError } from '@/error/UnauthorizedError';
+import { createUser, logUser } from '@/services/user.service';
 import type { Request, Response } from 'express';
 
 export const registerUser = async (req: Request, res: Response) => {
@@ -12,6 +14,36 @@ export const registerUser = async (req: Request, res: Response) => {
       data: user,
     });
   } catch (error) {
+    if (error instanceof ConflictError) {
+      return res.status(error.statusCode).send({
+        errorName: error.name,
+        error: error.message,
+      });
+    }
+
+    console.log(error);
+    res.status(500).send({
+      error: 'Internal Server Error',
+    });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const userData = req.body;
+    const user = await logUser(userData);
+
+    res.status(200).cookie('access_token', user, { httpOnly: true }).send({
+      message: 'User logged in successfully',
+    });
+  } catch (error) {
+    if (error instanceof UnauthorizedError) {
+      return res.status(error.statusCode).send({
+        errorName: error.name,
+        error: error.message,
+      });
+    }
+
     res.status(500).send({
       error: 'Internal Server Error',
     });
