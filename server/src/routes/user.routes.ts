c7c20624fd7824ad7820cdaf95a33e0979be19c4
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-03 23:08:20 -0300

    feat: add loginUser to userRouter

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,10 +1,12 @@
-import { registerUser } from '@/controllers/user.controller';
+import { registerUser, loginUser } from '@/controllers/user.controller';
+import { authMiddleware } from '@/middleware/auth/auth.middleware';
 import { validateData } from '@/middleware/validation/validation.middleware';
-import { userRegisterSchema } from '@/schemas/user.schema';
+import { userLoginSchema, userRegisterSchema } from '@/schemas/user.schema';
 import { Router } from 'express';
 
 const userRouter: Router = Router();
 
 userRouter.post('/register', validateData(userRegisterSchema), registerUser);
+userRouter.post('/login', validateData(userLoginSchema), loginUser);
 
 export { userRouter };
