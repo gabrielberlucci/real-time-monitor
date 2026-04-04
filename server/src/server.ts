da37c57b01a2d07eef9b86b3548d57a0bf64f28a
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-04 00:28:27 -0300

    feat: add use of cookie-parser

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import { routes } from './routes';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(routes);
 
 const port = process.env.PORT || 3000;
