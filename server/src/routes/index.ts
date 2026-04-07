bcd364f15ba0fe2984fb72d4312f0db445871484
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-07 01:31:07 -0300

    feat: add url api route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { userRouter } from './user.routes';
+import { urlRouter } from './url.router';
 
 const routes: Router = Router();
 
 routes.use('/api/user', userRouter);
+routes.use('/api/url', urlRouter);
 
 export { routes };
