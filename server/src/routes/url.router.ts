3d968c90f22acbe80376d02861790726c3a567c9
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-07 14:24:09 -0300

    feat: add data url validation middleware in create route

--- server/src/routes/url.router.ts
+++ server/src/routes/url.router.ts
@@ -1,9 +1,11 @@
 import { authMiddleware } from '@/middleware/auth/auth.middleware';
 import { url } from '@/controllers/url.controller';
 import { Router } from 'express';
+import { validateData } from '@/middleware/validation/validation.middleware';
+import { urlCreateSchema } from '@/schemas/url.schema';
 
 const urlRouter: Router = Router();
 
-urlRouter.post('/create', authMiddleware, url);
+urlRouter.post('/create', validateData(urlCreateSchema), authMiddleware, url);
 
 export { urlRouter };
