1eac500b243e4ae915264a33895d0f1ea6c5b202
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-07 00:54:57 -0300

    feat: remove unused auth middleware import

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,5 +1,4 @@
 import { registerUser, loginUser } from '@/controllers/user.controller';
-import { authMiddleware } from '@/middleware/auth/auth.middleware';
 import { validateData } from '@/middleware/validation/validation.middleware';
 import { userLoginSchema, userRegisterSchema } from '@/schemas/user.schema';
 import { Router } from 'express';
