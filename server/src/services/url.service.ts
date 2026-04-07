12db85761fff2583fb8db7458390aa1483f01d9e
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-07 01:32:00 -0300

    feat: add simple and not validate(yet) createUrl service

---
+++ server/src/services/url.service.ts
@@ -0,0 +1,17 @@
+import { prisma } from '@/lib/prisma';
+import type { Url } from 'generated/prisma/client';
+
+export const createUrl = async (url: string, userId: string) => {
+  try {
+    const urlData = prisma.url.create({
+      data: {
+        urlLink: url,
+        userId: userId,
+      },
+    });
+
+    return urlData;
+  } catch (error) {
+    throw error;
+  }
+};
