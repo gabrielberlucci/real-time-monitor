e756e987ea332de2d65b4d02f34d1c278c26c0f0
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-07 01:32:20 -0300

    feat: add url controlller

---
+++ server/src/controllers/url.controller.ts
@@ -0,0 +1,24 @@
+import { createUrl } from '@/services/url.service';
+import type { Request, Response } from 'express';
+
+export const url = async (req: Request, res: Response) => {
+  try {
+    const urlData = req.body;
+    const userId = req.user?.id;
+
+    console.log(userId);
+
+    const url = await createUrl(urlData.urlLink, userId!);
+
+    res.status(200).send({
+      message: 'Url created with success',
+      urlData: url,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).send({
+      message: 'Internal server error',
+    });
+  }
+};
